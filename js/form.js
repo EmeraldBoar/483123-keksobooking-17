116bb2c372b3603a33d1c9927eeb76a38e13f597
EmeraldBoar <kozlovkv174@gmail.com>
2019-08-03 18:28:15 +0400

    Связывает поля формы Количество гостей и комнат

--- js/form.js
+++ js/form.js
@@ -28,6 +28,36 @@
     }
   };
 
+  // Связь полей формы Количество комнат и гостей
+  var roomsSelect = adForm.querySelector('#room_number');
+  var guestsSelect = adForm.querySelector('#capacity');
+  guestsSelect.value = '1';
+
+  roomsSelect.addEventListener('change', function () {
+    for (var j = 0; j < guestsSelect.children.length; j++) {
+      guestsSelect.children[j].disabled = true;
+    }
+
+    if (roomsSelect.value === '1') {
+      guestsSelect.querySelector('option[value="1"]').disabled = false;
+    } else if (roomsSelect.value === '2') {
+      guestsSelect.querySelector('option[value="1"]').disabled = false;
+      guestsSelect.querySelector('option[value="2"]').disabled = false;
+    } else if (roomsSelect.value === '3') {
+      guestsSelect.querySelector('option[value="1"]').disabled = false;
+      guestsSelect.querySelector('option[value="2"]').disabled = false;
+      guestsSelect.querySelector('option[value="3"]').disabled = false;
+    } else {
+      guestsSelect.querySelector('option[value="0"]').disabled = false;
+    }
+
+    if (roomsSelect.value !== '100') {
+      guestsSelect.value = '1';
+    } else {
+      guestsSelect.value = '0';
+    }
+  });
+
   selectAccommodationType.addEventListener('change', getChangePrice);
   selectTimeOut.addEventListener('change', setTimeOut);
   selectTimeIn.addEventListener('change', setTimeIn);
